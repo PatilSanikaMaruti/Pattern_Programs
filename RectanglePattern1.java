10172c1a090d8adb38f922111b8422ae0ac39cc3
PatilSanikaMaruti <patilsanika20102003@gmail.com>
2025-02-13 23:24:37 +0530

    This is pattern programs

---
+++ RectanglePattern1.java
@@ -0,0 +1,15 @@
+class RectanglePattern1
+{
+	public static void main(String [] args)
+	{
+		int n=4;
+		for(int j=0;j<n-1;j++)
+		{
+			for(int i=0;i<n;i++)
+			{
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ RectanglePattern2.java
@@ -0,0 +1,15 @@
+class RectanglePattern2
+{
+	public static void main(String [] args)
+	{
+		int n=4;
+		for(int j=0;j<n;j++)
+		{
+			for(int i=0;i<n-1;i++)
+			{
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+}
